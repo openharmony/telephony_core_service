e0c6d1ba823aa897e30e425258a21615380d5dc6
lihongjin <lihongjin1@huawei.com>
2022-03-09 15:05:52 +0800

    fix: state_registry_notify 部件IPC client与server拆分
    Signed-off-by: lihongjin <lihongjin1@huawei.com>
    Change-Id: Iad9538b12a4fa5120d841f606a737e50ec306771

---
+++ frameworks/native/src/telephony_state_registry_client.cpp
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "telephony_state_registry_client.h"
+
+#include "if_system_ability_manager.h"
+#include "iservice_registry.h"
+#include "system_ability_definition.h"
+
+#include "telephony_log_wrapper.h"
+#include "state_registry_errors.h"
+
+namespace OHOS {
+namespace Telephony {
+TelephonyStateRegistryClient::TelephonyStateRegistryClient() = default;
+TelephonyStateRegistryClient::~TelephonyStateRegistryClient() = default;
+
+sptr<ITelephonyStateNotify> TelephonyStateRegistryClient::GetProxy()
+{
+    std::lock_guard<std::mutex> lock(mutexProxy_);
+    if (proxy_ != nullptr) {
+        return proxy_;
+    }
+
+    sptr<ISystemAbilityManager> sam = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+    if (sam == nullptr) {
+        TELEPHONY_LOGE("Failed to get system ability manager");
+        return nullptr;
+    }
+    sptr<IRemoteObject> obj = sam->CheckSystemAbility(TELEPHONY_STATE_REGISTRY_SYS_ABILITY_ID);
+    if (obj == nullptr) {
+        TELEPHONY_LOGE("Failed to get state registry service");
+        return nullptr;
+    }
+    std::unique_ptr<StateRegistryDeathRecipient> recipient = std::make_unique<StateRegistryDeathRecipient>(*this);
+    if (recipient == nullptr) {
+        TELEPHONY_LOGE("recipient is null");
+        return nullptr;
+    }
+    sptr<IRemoteObject::DeathRecipient> dr(recipient.release());
+    if ((obj->IsProxyObject()) && (!obj->AddDeathRecipient(dr))) {
+        TELEPHONY_LOGE("Failed to add death recipient");
+        return nullptr;
+    }
+    proxy_ = iface_cast<ITelephonyStateNotify>(obj);
+    deathRecipient_ = dr;
+    TELEPHONY_LOGI("Succeed to connect state registry service %{public}d", proxy_ == nullptr);
+    return proxy_;
+}
+
+void TelephonyStateRegistryClient::OnRemoteDied(const wptr<IRemoteObject> &remote)
+{
+    if (remote == nullptr) {
+        TELEPHONY_LOGE("OnRemoteDied failed, remote is nullptr");
+        return;
+    }
+    std::lock_guard<std::mutex> lock(mutexProxy_);
+    if (proxy_ == nullptr) {
+        TELEPHONY_LOGE("OnRemoteDied proxy_ is nullptr");
+        return;
+    }
+    auto serviceRemote = proxy_->AsObject();
+    if ((serviceRemote != nullptr) && (serviceRemote == remote.promote())) {
+        serviceRemote->RemoveDeathRecipient(deathRecipient_);
+        proxy_ = nullptr;
+        TELEPHONY_LOGE("on remote died");
+    }
+}
+
+int32_t TelephonyStateRegistryClient::UpdateCellularDataConnectState(
+    int32_t slotId, int32_t dataState, int32_t networkState)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateCellularDataConnectState(slotId, dataState, networkState);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateCellularDataFlow(int32_t slotId, int32_t dataFlowType)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateCellularDataFlow(slotId, dataFlowType);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateCallState(
+    int32_t slotId, int32_t callStatus, const std::u16string &number)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateCallState(slotId, callStatus, number);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateCallStateForSlotId(
+    int32_t slotId, int32_t callId, int32_t callStatus, const std::u16string &number)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateCallStateForSlotId(slotId, callId, callStatus, number);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateSignalInfo(
+    int32_t slotId, const std::vector<sptr<SignalInformation>> &vec)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateSignalInfo(slotId, vec);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateCellInfo(
+    int32_t slotId, const std::vector<sptr<CellInformation>> &vec)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateCellInfo(slotId, vec);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateNetworkState(
+    int32_t slotId, const sptr<NetworkState> &networkState)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateNetworkState(slotId, networkState);
+}
+
+int32_t TelephonyStateRegistryClient::UpdateSimState(
+    int32_t slotId, CardType type, SimState state, LockReason reason)
+{
+    auto proxy = GetProxy();
+    if (proxy == nullptr) {
+        TELEPHONY_LOGE("proxy is null!");
+        return TELEPHONY_ERR_IPC_CONNECT_STUB_FAIL;
+    }
+    return proxy->UpdateSimState(slotId, type, state, reason);
+}
+}
+}
+
---
+++ interfaces/innerkits/include/telephony_state_registry_proxy.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TELEPHONY_STATE_REGISTRY_PROXY_H
+#define TELEPHONY_STATE_REGISTRY_PROXY_H
+
+#include "iremote_proxy.h"
+
+#include "i_telephony_state_notify.h"
+
+namespace OHOS {
+namespace Telephony {
+class TelephonyStateRegistryProxy : public IRemoteProxy<ITelephonyStateNotify> {
+public:
+    explicit TelephonyStateRegistryProxy(const sptr<IRemoteObject> &impl)
+        : IRemoteProxy<ITelephonyStateNotify>(impl)
+    {}
+
+    virtual ~TelephonyStateRegistryProxy() = default;
+
+    int32_t UpdateCellularDataConnectState(
+        int32_t slotId, int32_t dataState, int32_t networkState) override;
+    int32_t UpdateCellularDataFlow(
+        int32_t slotId, int32_t dataFlowType) override;
+    int32_t UpdateCallState(
+        int32_t slotId, int32_t callStatus, const std::u16string &number) override;
+    int32_t UpdateCallStateForSlotId(
+        int32_t slotId, int32_t callId, int32_t callStatus, const std::u16string &number) override;
+    int32_t UpdateSignalInfo(
+        int32_t slotId, const std::vector<sptr<SignalInformation>> &vec) override;
+    int32_t UpdateCellInfo(
+        int32_t slotId, const std::vector<sptr<CellInformation>> &vec) override;
+    int32_t UpdateNetworkState(
+        int32_t slotId, const sptr<NetworkState> &networkState) override;
+    int32_t UpdateSimState(
+        int32_t slotId, CardType type, SimState state,  LockReason reason) override;
+
+    int32_t RegisterStateChange(const sptr<TelephonyObserverBroker> &telephonyObserver,
+        int32_t slotId, uint32_t mask, bool isUpdate) override;
+    int32_t UnregisterStateChange(int32_t slotId, uint32_t mask) override;
+
+private:
+    static inline BrokerDelegator<TelephonyStateRegistryProxy> delegator_;
+};
+} // namespace Telephony
+} // namespace OHOS
+#endif // TELEPHONY_STATE_REGISTRY_PROXY_H
