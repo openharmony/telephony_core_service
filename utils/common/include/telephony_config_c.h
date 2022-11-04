7585f2bd99d42cf040ff2eaa8238a0c957067386
dingxiaochen <dingxiaochen@huawei.com>
2022-11-04 10:10:28 +0800

    add carrier config.
    Signed-off-by: dingxiaochen <dingxiaochen@huawei.com>

---
+++ utils/common/include/telephony_config_c.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef TELEPHONY_CONFIG_C_H
+#define TELEPHONY_CONFIG_C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// opkey info for sim0
+#define CUST_OPKEY0 "telephony.sim.opkey0"
+// opkey info for sim1
+#define CUST_OPKEY1 "telephony.sim.opkey1"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TELEPHONY_CONFIG_C_H
