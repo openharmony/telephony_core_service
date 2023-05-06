6a7c3c5ba7280047a325657d0d8885ccc89841e6
dingxiaochen <dingxiaochen@huawei.com>
2023-05-06 14:17:41 +0800

    part dependence.
    Signed-off-by: dingxiaochen <dingxiaochen@huawei.com>

--- test/fuzztest/sendenvelopecmd_fuzzer/sendenvelopecmd_fuzzer.cpp
+++ test/fuzztest/sendenvelopecmd_fuzzer/sendenvelopecmd_fuzzer.cpp
@@ -38,6 +38,7 @@ bool IsServiceInited()
     if (!g_isInited) {
         auto onStart = [] { DelayedSingleton<CoreService>::GetInstance()->OnStart(); };
         std::thread startThread(onStart);
+        pthread_setname_np(startThread.native_handle(), "sendenvelopecmd_fuzzer");
         startThread.join();
 
         sleep(SLEEP_TIME_SECONDS);
