329f7d7e3c1392c817bc00acb549588e465172dd
xieyao_whu <xieyao7@huawei.com>
2023-11-16 01:27:42 +0000

    update services/sim/src/sim_manager.cpp.
    Signed-off-by: xieyao_whu <xieyao7@huawei.com>

--- services/sim/src/sim_manager.cpp
+++ services/sim/src/sim_manager.cpp
@@ -605,6 +605,10 @@ int32_t SimManager::SimAuthentication(
 int32_t SimManager::SendSimMatchedOperatorInfo(
     int32_t slotId, int32_t state, const std::string &operName, const std::string &operKey)
 {
+    if (!HasSimCardInner(slotId)) {
+        TELEPHONY_LOGE("SendSimMatchedOperatorInfo has no sim card!");
+        return TELEPHONY_ERR_NO_SIM_CARD;
+    }
     if (simStateManager_[slotId] == nullptr) {
         TELEPHONY_LOGE("simStateManager_ can not be null!");
         return TELEPHONY_ERR_LOCAL_PTR_NULL;
