d2e61c176bc69beb32a4b694b703d1c1a6bc8ac1
Aurora <liuxiyao223@huawei.com>
2024-02-04 09:42:28 +0000

    update services/sim/src/icc_file_controller.cpp.
    Signed-off-by: Aurora <liuxiyao223@huawei.com>

--- services/sim/src/icc_file_controller.cpp
+++ services/sim/src/icc_file_controller.cpp
@@ -92,7 +92,7 @@ void IccFileController::ProcessRecordSize(const AppExecFwk::InnerEvent::Pointer 
     std::string str = IccFileController::NULLSTR;
     std::string path = IccFileController::NULLSTR;
     std::unique_ptr<IccFromRilMsg> rcvMsg = event->GetUniqueObject<IccFromRilMsg>();
-    if (rcvMsg == nullptr && rcvMsg->controlHolder == nullptr) {
+    if (rcvMsg == nullptr || rcvMsg->controlHolder == nullptr) {
         TELEPHONY_LOGE("rcvMsg or controlHolder is nullptr");
         return;
     }
