b56ee2caad6881dd1251e2d3677db1c764301908
liuxiyao223 <liuxiyao223@huawei.com>
2024-03-18 15:20:22 +0800

    日志轻量化整改
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- utils/log/include/telephony_log_wrapper.h
+++ utils/log/include/telephony_log_wrapper.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -33,11 +33,18 @@ namespace Telephony {
 #define TELEPHONY_LOG_FUNC_NAME __func__
 #endif
 
-static constexpr OHOS::HiviewDFX::HiLogLabel TELEPHONY_LABEL = {LOG_CORE, LOG_DOMAIN, TELEPHONY_LOG_TAG};
-
 #define OHOS_DEBUG
 #ifndef OHOS_DEBUG
-#define PRINT_TELEPHONY_LOG(op, fmt, ...) (void)OHOS::HiviewDFX::HiLog::op(TELEPHONY_LABEL, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGE(fmt, ...) \
+    (void)HILOG_IMPL(LOG_CORE, LOG_ERROR, LOG_DOMAIN, TELEPHONY_LOG_TAG, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGW(fmt, ...) \
+    (void)HILOG_IMPL(LOG_CORE, LOG_WARN, LOG_DOMAIN, TELEPHONY_LOG_TAG, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGI(fmt, ...) \
+    (void)HILOG_IMPL(LOG_CORE, LOG_INFO, LOG_DOMAIN, TELEPHONY_LOG_TAG, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGF(fmt, ...) \
+    (void)HILOG_IMPL(LOG_CORE, LOG_FATAL, LOG_DOMAIN, TELEPHONY_LOG_TAG, fmt, ##__VA_ARGS__)
+#define TELEPHONY_LOGD(fmt, ...) \
+    (void)HILOG_IMPL(LOG_CORE, LOG_DEBUG, LOG_DOMAIN, TELEPHONY_LOG_TAG, fmt, ##__VA_ARGS__)
 #else
 // Gets the raw file name of the file.
 // This function is a function executed by the compiler, that is,
@@ -59,16 +66,22 @@ inline constexpr const char *GetRawFileName(const char *path)
     return (*start == ch) ? ++start : path;
 }
 
-#define PRINT_TELEPHONY_LOG(op, fmt, ...)                                                                        \
-    (void)OHOS::HiviewDFX::HiLog::op(TELEPHONY_LABEL, "[%{public}s-(%{public}s:%{public}d)] " fmt, __FUNCTION__, \
-        GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
+#define TELEPHONY_LOGE(fmt, ...)                                                                                      \
+    (void)HILOG_IMPL(LOG_CORE, LOG_ERROR, LOG_DOMAIN, TELEPHONY_LOG_TAG, "[%{public}s-(%{public}s:%{public}d)] " fmt, \
+    __FUNCTION__, GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
+#define TELEPHONY_LOGW(fmt, ...)                                                                                     \
+    (void)HILOG_IMPL(LOG_CORE, LOG_WARN, LOG_DOMAIN, TELEPHONY_LOG_TAG, "[%{public}s-(%{public}s:%{public}d)] " fmt, \
+    __FUNCTION__, GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
+#define TELEPHONY_LOGI(fmt, ...)                                                                                     \
+    (void)HILOG_IMPL(LOG_CORE, LOG_INFO, LOG_DOMAIN, TELEPHONY_LOG_TAG, "[%{public}s-(%{public}s:%{public}d)] " fmt, \
+    __FUNCTION__, GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
+#define TELEPHONY_LOGF(fmt, ...)                                                                                      \
+    (void)HILOG_IMPL(LOG_CORE, LOG_FATAL, LOG_DOMAIN, TELEPHONY_LOG_TAG, "[%{public}s-(%{public}s:%{public}d)] " fmt, \
+    __FUNCTION__, GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
+#define TELEPHONY_LOGD(fmt, ...)                                                                                      \
+    (void)HILOG_IMPL(LOG_CORE, LOG_DEBUG, LOG_DOMAIN, TELEPHONY_LOG_TAG, "[%{public}s-(%{public}s:%{public}d)] " fmt, \
+    __FUNCTION__, GetRawFileName(__FILE__), __LINE__, ##__VA_ARGS__)
 #endif
-
-#define TELEPHONY_LOGE(fmt, ...) PRINT_TELEPHONY_LOG(Error, fmt, ##__VA_ARGS__)
-#define TELEPHONY_LOGW(fmt, ...) PRINT_TELEPHONY_LOG(Warn, fmt, ##__VA_ARGS__)
-#define TELEPHONY_LOGI(fmt, ...) PRINT_TELEPHONY_LOG(Info, fmt, ##__VA_ARGS__)
-#define TELEPHONY_LOGF(fmt, ...) PRINT_TELEPHONY_LOG(Fatal, fmt, ##__VA_ARGS__)
-#define TELEPHONY_LOGD(fmt, ...) PRINT_TELEPHONY_LOG(Debug, fmt, ##__VA_ARGS__)
 #endif // CONFIG_HILOG
 } // namespace Telephony
 } // namespace OHOS
