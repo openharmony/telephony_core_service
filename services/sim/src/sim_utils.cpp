dd1e2c85e93256618931acccf83e0be9c6a2fb9e
jierbumo866 <fangping5@huawei.com>
2024-07-17 20:22:08 +0800

    UcsConvertToString crash异常处理
    Signed-off-by: jierbumo866 <fangping5@huawei.com>

--- services/sim/src/sim_utils.cpp
+++ services/sim/src/sim_utils.cpp
@@ -233,7 +233,8 @@ std::u16string SIMUtils::UcsConvertToString(unsigned char *data, int length, int
     if (len > length - START_POS) {
         len = length - START_POS;
     }
-    unsigned char dataUsc[MAX_ENGLISH_NAME * HALF_LEN] = { FF_DATA };
+    TELEPHONY_LOGI("len2: %{public}d", len);
+    unsigned char* dataUsc = new unsigned char[len * HALF_LEN]{ FF_DATA };
     int index = 0;
     int base = 0;
     int dataOffset = UCS_OFFSET;
@@ -251,6 +252,8 @@ std::u16string SIMUtils::UcsConvertToString(unsigned char *data, int length, int
     }
     int outlen = 0;
     std::shared_ptr<char16_t> cs = CharsConvertToChar16(dataUsc, MAX_ENGLISH_NAME * HALF_LEN, outlen, true);
+    delete[] dataUsc;
+    dataUsc = nullptr;
     if (cs == nullptr) {
         TELEPHONY_LOGE("cs is nullptr");
         return u"";
@@ -268,7 +271,7 @@ std::u16string SIMUtils::UcsWideConvertToString(unsigned char *data, int length,
         len = length - END_POS;
     }
     int base = (data[offset + UCS_BASE_POS] << BYTE_BIT) + data[offset + UCS_BASE_POS + 1];
-    unsigned char dataUsc[MAX_ENGLISH_NAME * HALF_LEN] = { FF_DATA };
+    unsigned char* dataUsc = new unsigned char[len * HALF_LEN]{ FF_DATA };
     int dataOffset = UCS_WIDE_OFFSET;
     int index = 0;
     while (index < len * HALF_LEN && offset + dataOffset < length) {
@@ -285,6 +288,8 @@ std::u16string SIMUtils::UcsWideConvertToString(unsigned char *data, int length,
     }
     int outlen = 0;
     std::shared_ptr<char16_t> cs = CharsConvertToChar16(dataUsc, MAX_ENGLISH_NAME * HALF_LEN, outlen, true);
+    delete[] dataUsc;
+    dataUsc = nullptr;
     if (cs == nullptr) {
         TELEPHONY_LOGE("cs is nullptr");
         return u"";
