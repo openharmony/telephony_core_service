2c499a17d689974fa5f72cd34dd89e44d548f172
wanx194 <wanxing21@huawei.com>
2024-08-02 12:04:25 +0000

    resolve coreservice crash when use copyoperatorconfig
    Signed-off-by: wanx194 <wanxing21@huawei.com>

--- services/sim/src/operator_config_cache.cpp
+++ services/sim/src/operator_config_cache.cpp
@@ -43,6 +43,14 @@ void OperatorConfigCache::ClearAllCache(int32_t slotId)
     lock.unlock();
 }
 
+void OperatorConfigCache::ClearMemoryAndOpkey(int32_t slotId)
+{
+    std::unique_lock<std::mutex> lock(mutex_);
+    ClearOperatorValue(slotId);
+    ClearMemoryCache(slotId);
+    lock.unlock();
+}
+
 void OperatorConfigCache::ClearOperatorValue(int32_t slotId)
 {
     auto simFileManager = simFileManager_.lock();
