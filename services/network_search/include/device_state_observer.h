df6757aa16b2fa223f404afd381f870b0f9a9c9b
陈煜 <417887319@qq.com>
2024-08-20 09:26:54 +0000

    update services/network_search/include/device_state_observer.h. build
    Signed-off-by: 陈煜 <417887319@qq.com>

--- services/network_search/include/device_state_observer.h
+++ services/network_search/include/device_state_observer.h
@@ -91,9 +91,7 @@ public:
 
 private:
     std::shared_ptr<DeviceStateEventSubscriber> subscriber_;
-#ifdef ABILITY_NETMANAGER_EXT_SUPPORT
     sptr<NetManagerStandard::ISharingEventCallback> sharingEventCallback_ = nullptr;
-#endif
     sptr<ISystemAbilityStatusChange> statusChangeListener_ = nullptr;
 
 #ifdef ABILITY_NETMANAGER_EXT_SUPPORT
