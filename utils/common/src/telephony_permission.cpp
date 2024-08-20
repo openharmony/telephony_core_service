d922c4bc67bc61e1acde527bc5983871c9a532bb
panlulu99 <panlulu11@huawei.com>
2024-08-20 11:33:23 +0800

    Modify log level from debug to error
    Signed-off-by: panlulu99 <panlulu11@huawei.com>

--- utils/common/src/telephony_permission.cpp
+++ utils/common/src/telephony_permission.cpp
@@ -89,8 +89,9 @@ bool TelephonyPermission::CheckPermission(const std::string &permissionName)
     }
 
     if (result != PermissionState::PERMISSION_GRANTED) {
-        TELEPHONY_LOGD("permission check failed, permission:%{public}s, callerToken:%{public}u, tokenType:%{public}d",
-            permissionName.c_str(), callerToken, tokenType);
+        TELEPHONY_LOGE("permission check failed, permission:%{public}s, callerToken:%{public}u, "
+            "tokenType:%{public}d, pid:%{public}d",
+            permissionName.c_str(), callerToken, tokenType, IPCSkeleton::GetCallingPid());
         return false;
     }
     return true;
