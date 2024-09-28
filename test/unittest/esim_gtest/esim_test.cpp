160229caf2243ace3d0d3627402963b201d4ae03
wangxingboo <wangxingbo11@h-partners.com>
2024-09-28 16:27:56 +0800

    fix codex
    Signed-off-by: wangxingboo <wangxingbo11@h-partners.com>

--- test/unittest/esim_gtest/esim_test.cpp
+++ test/unittest/esim_gtest/esim_test.cpp
@@ -108,7 +108,8 @@ HWTEST_F(EsimTest, ProcessObtainDefaultSmdpAddress_001, Function | MediumTest | 
     std::shared_ptr<Telephony::EsimFile> esimFile = std::make_shared<EsimFile>(simStateManager);
     int slotId = 0;
     std::shared_ptr<Telephony::IccFile> iccFile = std::make_shared<EsimFile>(simStateManager);
-    AppExecFwk::InnerEvent::Pointer eventSmdpAddress = iccFile->BuildCallerInfo(MSG_ESIM_OBTAIN_DEFAULT_SMDP_ADDRESS_DONE);
+    AppExecFwk::InnerEvent::Pointer eventSmdpAddress =
+        iccFile->BuildCallerInfo(MSG_ESIM_OBTAIN_DEFAULT_SMDP_ADDRESS_DONE);
     esimFile->currentChannelId = 0;
     EXPECT_FALSE(esimFile->ProcessObtainDefaultSmdpAddress(slotId, eventSmdpAddress));
     esimFile->currentChannelId = 2;
@@ -205,17 +206,17 @@ HWTEST_F(EsimTest, ProcessGetProfileDone_001, Function | MediumTest | Level2)
     std::shared_ptr<IccControllerHolder> holder = nullptr;
     std::unique_ptr<Telephony::IccFromRilMsg> rcvMsg = std::make_unique<Telephony::IccFromRilMsg>(holder);
     rcvMsg->fileData.resultData = "BF2D8184A08181E37F"
-                                 "5A0A89670000000000452301" // ICCID
-                                 "90046E69636B" // Nickname
-                                 "9103746D6F" // Service provider name
-                                 "92027031" // Profile name
-                                 "B70F800312F34581030102038203040506" // Operator id
-                                 "9F700101" // Profile state
-                                 "950101" // Profile class
-                                 "990206C0" // Policy rules
+                                 "5A0A89670000000000452301"
+                                 "90046E69636B"
+                                 "9103746D6F"
+                                 "92027031"
+                                 "B70F800312F34581030102038203040506"
+                                 "9F700101"
+                                 "950101"
+                                 "990206C0"
                                  "BF7645E243E135C114ABCD92CBB156B280FA4E1429A6ECEEB6E5C1BFE4"
                                  "CA1D636F6D2E676F6F676C652E616E64726F69642E617070732E6D79617070"
-                                 "E30ADB080000000000000001" // Carrier privilege rules
+                                 "E30ADB080000000000000001"
                                  "9000";
     auto event = AppExecFwk::InnerEvent::Get(0, rcvMsg);
     EXPECT_TRUE(esimFile->ProcessGetProfileDone(event));
