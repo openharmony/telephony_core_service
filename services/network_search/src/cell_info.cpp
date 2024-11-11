49d043a7018b0a60bed0a03da847dbe9e8447ca8
rsin <renxin20@h-partners.com>
2024-11-11 07:14:45 +0000

    修复crash
    Signed-off-by: rsin <renxin20@h-partners.com>

--- services/network_search/src/cell_info.cpp
+++ services/network_search/src/cell_info.cpp
@@ -123,7 +123,8 @@ void CellInfo::ProcessCurrentCellInfo(const AppExecFwk::InnerEvent::Pointer &eve
         TELEPHONY_LOGE("CellInfo::ProcessCurrentCellInfo event is nullptr slotId:%{public}d", slotId_);
         return;
     }
-    std::shared_ptr<CellListCurrentInformation> cellInformationList = event->GetSharedObject<CellListCurrentInformation>();
+    std::shared_ptr<CellListCurrentInformation> cellInformationList = 
+        event->GetSharedObject<CellListCurrentInformation>();
     CellListCurrentInformation *cellInfoList = cellInformationList.get();
     if (cellInfoList == nullptr) {
         TELEPHONY_LOGE("CellInfo::ProcessCurrentCellInfo cellInfoList is nullptr slotId:%{public}d", slotId_);
