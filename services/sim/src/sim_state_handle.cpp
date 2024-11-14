89fcae92eedb6a27ebbeb571d1e6004c50792205
刘峻朋 <liujunpeng20@huawei.com>
2024-11-14 08:48:55 +0000

    cleancode
    Signed-off-by: 刘峻朋 <liujunpeng20@huawei.com>

--- services/sim/src/sim_state_handle.cpp
+++ services/sim/src/sim_state_handle.cpp
@@ -707,7 +707,7 @@ void SimStateHandle::ProcessEvent(const AppExecFwk::InnerEvent::Pointer &event)
         }
         if (eventId == MSG_SIM_SEND_NCFG_OPER_INFO_DONE) {
             simStateManager->SyncSimMatchResponse();
-        } else if(eventId == MSG_SIM_UNLOCK_PIN_DONE){
+        } else if (eventId == MSG_SIM_UNLOCK_PIN_DONE) {
             simStateManager->SyncUnlockPinResponse();
         } else {
             simStateManager->SyncCmdResponse();
