74a9c7f1ed2319871486bd2c1b84f29d797b1978
郑晓晴 <zhaoxiaoqing17@h-partners.com>
2024-11-26 20:00:08 +0800

    函数实现优化为callback形式
    Signed-off-by: 郑晓晴 <zhaoxiaoqing17@h-partners.com>

--- frameworks/js/esim/include/napi_esim.h
+++ frameworks/js/esim/include/napi_esim.h
@@ -34,7 +34,7 @@
 
 namespace OHOS {
 namespace Telephony {
-constexpr int WAIT_TIME_SECOND = 60 * 3;
+constexpr int WAIT_TIME_SECOND = 30;
 const int32_t DEFAULT_ERROR = -1;
 
 template<typename T>
