f8b44d48026467ba3e4c39afb157a124ab05197d
郑晓晴 <zhaoxiaoqing17@h-partners.com>
2024-11-29 11:30:01 +0800

    函数实现优化为callback形式
    Signed-off-by: 郑晓晴 <zhaoxiaoqing17@h-partners.com>

--- services/sim/src/multi_sim_controller.cpp
+++ services/sim/src/multi_sim_controller.cpp
@@ -544,13 +544,13 @@ int32_t MultiSimController::SetActiveSim(int32_t slotId, int32_t enable, bool fo
         return TELEPHONY_ERR_ARGUMENT_INVALID;
     }
 #else
-    if ((!IsValidData(slotId)) && (!simFileManager_[slotId]->IsEsimSupported())) {
+    if ((!IsValidData(slotId)) && (!simFileManager_[slotId]->IsSupported())) {
         TELEPHONY_LOGE("invalid slotid.");
         return TELEPHONY_ERR_NO_SIM_CARD;
     }
     int curSimId = 0;
     if (GetTargetSimId(slotId, curSimId) != TELEPHONY_ERR_SUCCESS) {
-        if (!simFileManager_[slotId]->IsEsimSupported()){
+        if (!simFileManager_[slotId]->IsSupported()){
             TELEPHONY_LOGE("failed by out of range");
             return TELEPHONY_ERR_ARGUMENT_INVALID;
         }
