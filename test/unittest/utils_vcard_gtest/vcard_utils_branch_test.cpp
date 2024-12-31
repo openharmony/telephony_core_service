ae00f3d71a6ef81f5a898f3ed9189aa905b523ec
汪少俊 <newstriving@126.com>
2024-12-31 13:18:31 +0000

    增加DT
    Signed-off-by: 汪少俊 <newstriving@126.com>

---
+++ test/unittest/utils_vcard_gtest/vcard_utils_branch_test.cpp
@@ -0,0 +1,17 @@
+#define private public
+#define protected public
+#incldue "base64.h"
+#incldue "vcard_utils.h"
+#include <fcntl.h>
+#include <iostream>
+#include <gtest/gtest.h>
+#include <string>
+
+namespace OHOS {
+namespace Telephony {
+using namespace testing::ext;
+
+class Utils
+
+} // namespace Telephony
+} // namespace OHOS
