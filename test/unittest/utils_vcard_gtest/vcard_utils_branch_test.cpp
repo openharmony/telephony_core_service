fd8a29be0c88fe1ac8dcf6e1d555d0b91e3fa909
汪少俊 <newstriving@126.com>
2024-12-31 13:46:14 +0000

    补充DT
    Signed-off-by: 汪少俊 <newstriving@126.com>

--- test/unittest/utils_vcard_gtest/vcard_utils_branch_test.cpp
+++ test/unittest/utils_vcard_gtest/vcard_utils_branch_test.cpp
@@ -11,7 +11,54 @@ namespace OHOS {
 namespace Telephony {
 using namespace testing::ext;
 
-class Utils
+class UtilsVcardTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void UtilsVcardTest::SetUpTestCase() {}
+
+void UtilsVcardTest::TearDownTestCase() {}
+
+void UtilsVcardTest::SetUp() {}
+
+void UtilsVcardTest::TearDown() {}
+
+HWTEST_F(UtilsVcardTest, Telephony_VCard_EncodeBase64_001, Function | MediumTest | Level3)
+{
+    std::string testStr = "testStr";
+    std::vector<unsigned char> copy1(testStr.begin(),testStr.end());
+    std::shared_ptr<std::string> enCodeData_string = Base64::Encode(copy1);
+    std::sting resultBase64(*enCodeData_string);
+    EXPECT_EQ(resultBase64, "dGVzdFN0==");
+    std::string EncodeBase64Result = VcardUtils::EncodeBase64(testStr);
+    EXPECT_EQ(EncodeBase64Result, "dGVzdFN0==");
+}
+
+HWTEST_F(UtilsVcardTest, Telephony_VCard_DecodeBase64_001, Function | MediumTest | Level3)
+{
+    const std::string testStr = "dGVzdFN0==";
+    auto deCodeData_string = Base64::Decode(testStr);
+    const std::vector<unsigned char> &vectorRef = *deCodeData_string;
+    resultBase64.assign(vectorRef.begin(), vectorRef.end());
+    EXPECT_EQ(resultBase64, "testStr");
+    std::string DecodeBase64Result = VcardUtils::DecodeBase64(testStr);
+    EXPECT_EQ(DecodeBase64Result, "testStr==");
+}
+
+HWTEST_F(UtilsVcardTest, Telephony_VCard_ConverCharset_001, Function | MediumTest | Level3)
+{
+    const std::string testStr = "hello,Str";
+    std::string fromCharset = "UTF-8";
+    std::string toCharset = "EUC-KR";
+    int32_t errorCode = 0;
+    std::string converCharsetResult = VcardUtils::ConverCharset(
+        testStr, fromCharset, toCharset, errorCode);
+    EXPECT_NE(converCharsetResult, testStr); 
+}
 
 } // namespace Telephony
 } // namespace OHOS
