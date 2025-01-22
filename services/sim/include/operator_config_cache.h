edeaa03ef8ddd09fd3f83c49adda209034ba9bdd
tongrui <tongrui236@163.com>
2025-01-22 17:45:55 +0800

    修复VoLTE开关不刷新问题
    Signed-off-by: 童睿 <tongrui7@huawei.com>

--- services/sim/include/operator_config_cache.h
+++ services/sim/include/operator_config_cache.h
@@ -53,10 +53,11 @@ private:
     std::weak_ptr<SimFileManager> simFileManager_;
     std::string GetOpKey(int32_t slotId);
     void CopyOperatorConfig(const OperatorConfig &from, OperatorConfig &to);
-    void UpdateCurrentOpc(int32_t slotId, OperatorConfig &poc, int32_t state, bool needUpdateLoading);
+    void UpdateCurrentOpc(int32_t slotId, OperatorConfig &poc);
     void SendSimMatchedOperatorInfo(int32_t slotId, int32_t state);
     bool AnnounceOperatorConfigChanged(int32_t slotId, int32_t state);
     void notifyInitApnConfigs(int32_t slotId);
+    int32_t LoadOperatorConfigWithoutAnnounce(int32_t slotId, OperatorConfig &poc);
     inline static const std::string KEY_SLOTID = "slotId";
     inline static const std::string CHANGE_STATE = "state";
     inline static const int32_t IMS_SWITCH_OFF = 0;
