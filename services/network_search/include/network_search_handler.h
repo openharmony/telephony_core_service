4afaf0eb53cad039abe1bd96648562f00873fbf1
zouxuebin <1593648@qq.com>
2025-02-17 08:59:55 +0000

    update services/network_search/include/network_search_handler.h.
    Signed-off-by: zouxuebin <1593648@qq.com>

--- services/network_search/include/network_search_handler.h
+++ services/network_search/include/network_search_handler.h
@@ -196,7 +196,7 @@ private:
         int32_t oldRadioState_;
         int32_t newRadioState_;
     };
-    std::unordered_map<int32_t, slotRadioStateChange> slotRadioStateChangeMap_ = { 
+    std::unordered_map<int32_t, slotRadioStateChange> slotRadioStateChangeMap_ = {
         { 0, {INIT_RADIO_STATE, INIT_RADIO_STATE} },
         { 1, {INIT_RADIO_STATE, INIT_RADIO_STATE} }
     };
