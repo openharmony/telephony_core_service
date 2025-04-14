43aee02a4539b9eb895b1cd9bc2330486ad1ed86
李江彤 <1332187805@qq.com>
2025-04-14 14:17:54 +0000

    update interfaces/innerkits/include/telephony_types.h.
    Signed-off-by: 李江彤 <1332187805@qq.com>

--- interfaces/innerkits/include/telephony_types.h
+++ interfaces/innerkits/include/telephony_types.h
@@ -117,7 +117,6 @@ inline T GetDefaultEsimSlotId()
     if (esimDefaultSlotId_ == ESIM_DEFAULT_SLOTID) {
         char esimDefaultSlotId[SYSPARA_SIZE] = { 0 };
         GetParameter(TEL_DEFAULT_ESIM_SLOT_ID, DEFAULT_ESIM_SLOT_ID, esimDefaultSlotId, SYSPARA_SIZE);
-        esimDefaultSlotId_ = std::stoi(esimDefaultSlotId);
         ConvertStrToInt(esimDefaultSlotId, esimDefaultSlotId_, DEC_TYPE);
     }
     return esimDefaultSlotId_;
