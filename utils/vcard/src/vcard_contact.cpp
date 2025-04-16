3316640ff99805557c7bdcaa8ae9eade728992c5
刘荣亮 <liurongliang2@h-partners.com>
2025-04-16 06:55:54 +0000

    update utils/vcard/src/vcard_contact.cpp.
    Signed-off-by: 刘荣亮 <liurongliang2@h-partners.com>

--- utils/vcard/src/vcard_contact.cpp
+++ utils/vcard/src/vcard_contact.cpp
@@ -166,7 +166,7 @@ void VCardContact::CheckNameExist()
  
 int32_t VCardContact::BuildContactData(int32_t rawId, std::vector<DataShare::DataShareValuesBucket> &contactDataValues)
 {
-    if (nameData_->GetDisplayName().empty()) {
+    if (nameData_ != nullptr && nameData_->GetDisplayName().empty()) {
         UpdateDisplayName();
         CheckNameExist();
     }
