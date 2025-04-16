370dc55fc16f9682227e15b2eafa1cfff9b4dfaa
zouxuebin <1593648@qq.com>
2025-04-16 07:35:20 +0000

    update services/sim/src/sim_file.cpp.
    Signed-off-by: zouxuebin <1593648@qq.com>

--- services/sim/src/sim_file.cpp
+++ services/sim/src/sim_file.cpp
@@ -1129,7 +1129,6 @@ bool SimFile::ProcessGetAdDone(const AppExecFwk::InnerEvent::Pointer &event)
     } else if (doneData && (lengthOfMnc_ != MNC_LEN) && (lengthOfMnc_ != MCC_LEN)) {
         lengthOfMnc_ = UNINITIALIZED_MNC;
     }
-    SaveCountryCode();
     TELEPHONY_LOGI("update5 length Mnc_= %{public}d", lengthOfMnc_);
     CheckMncLengthForAdDone();
     return isFileProcessResponse;
