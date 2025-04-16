30a67d4447252afe363d28fb138e66994543deeb
TingweiCHEN <chentingwei5@huawei.com>
2025-04-16 12:23:13 +0800

    fix crash when parse bytes into int
    Signed-off-by: TingweiCHEN <chentingwei5@huawei.com>

--- test/unittest/utils_codec_gtest/reset_reponse_test.cpp
+++ test/unittest/utils_codec_gtest/reset_reponse_test.cpp
@@ -20,12 +20,14 @@
 #include "reset_response.h"
 #include "asn1_node.h"
 #include "telephony_log_wrapper.h"
-
+#define private public
+#define protected public
 using namespace testing::ext;
 namespace OHOS {
 namespace Telephony {
 #ifndef TEL_TEST_UNSUPPORT
-
+const uint32_t MAX_BPP_LENGTH = 245760;
+const uint32_t BYTE_TO_HEX_LEN = 2;
 class ResetResponseTest : public testing::Test {
 public:
     static void SetUpTestCase();
@@ -47,13 +49,22 @@ HWTEST_F(ResetResponseTest, ParseAtrData_ValidInput, Function | MediumTest | Lev
     ResetResponse response;
     const std::string atr1 = "";
     EXPECT_FALSE(response.AnalysisAtrData(atr1));
-    const std::string atr2 = "123";
+    const std::string atr2 = "12";
     EXPECT_FALSE(response.AnalysisAtrData(atr2));
-    const std::string atr3 = "12";
+    const std::string atr3 = "123";
     EXPECT_FALSE(response.AnalysisAtrData(atr3));
-    const std::string atr = "3B9F96803F47A28031E073FE211B6759868828681011C4";
-    EXPECT_TRUE(response.AnalysisAtrData(atr));
+        const std::string atr4 = "1234";
+    EXPECT_FALSE(response.AnalysisAtrData(atr4));
+    const std::string atr5 = "3B9F96803F47A28031E073FE211B6759868828681011C4";
+    EXPECT_TRUE(response.AnalysisAtrData(atr5));
     EXPECT_TRUE(response.IsEuiccAvailable());
+    const std::string atr6 = "4B4F96803F47A28031E073FE211B6759868828681011C4";
+    EXPECT_FALSE(response.AnalysisAtrData(atr6));
+    std::string atr7 = "";
+    for (int i = 0; i < MAX_BPP_LENGTH * BYTE_TO_HEX_LEN + 2; i++) {
+        atr7 += "0";
+    }
+    EXPECT_FALSE(response.AnalysisAtrData(atr7));
 }
 
 HWTEST_F(ResetResponseTest, CheckAtrDataParam_ValidInput, Function | MediumTest | Level1)
@@ -83,6 +94,15 @@ HWTEST_F(ResetResponseTest, CheckIsEuiccAvailable_ValidInput, Function | MediumT
     charB = 0xA2;
     charD = 0x3F;
     EXPECT_TRUE(response.CheckIsEuiccAvailable(charB, charD));
+}
+
+HWTEST_F(ResetResponseTest, AnalysisInterfaceData_ValidInput, Function | MediumTest | Level1)
+{
+    ResetResponse response;
+    std::vector<uint8_t> atrData;
+    uint32_t atrDataLen = 0;
+    uint32_t index = 0;
+    EXPECT_TRUE(response.AnalysisInterfaceData(atrData, atrDataLen, index));
 }
 #endif // TEL_TEST_UNSUPPORT
 } // namespace Telephony
