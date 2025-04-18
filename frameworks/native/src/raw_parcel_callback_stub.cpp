6fdc9544e6cbffb6437002f89baf3c69b2a8a9eb
xhttt123 <2317512661@qq.com>
2025-04-18 05:03:20 +0000

    add frameworks/native/src/raw_parcel_callback_stub.cpp.
    Signed-off-by: xhttt123 <2317512661@qq.com>

---
+++ frameworks/native/src/raw_parcel_callback_stub.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2024-2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "raw_parcel_callback_stub.h"
+#include "telephony_log_wrapper.h"
+#include "telephony_errors.h"
+ 
+namespace OHOS {
+namespace Telephony {
+RawParcelCallbackStub::RawParcelCallbackStub(std::function<void(MessageParcel &data, MessageParcel &reply)> callback)
+    : callback_(callback)
+{
+}
+
+int RawParcelCallbackStub::OnRemoteRequest(
+    uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option)
+{
+    std::u16string myDescriptor = RawParcelCallbackStub::GetDescriptor();
+    std::u16string remoteDescriptor = data.ReadInterfaceToken();
+    if (myDescriptor != remoteDescriptor) {
+        TELEPHONY_LOGE("descriptor check fail!");
+        return TELEPHONY_ERR_DESCRIPTOR_MISMATCH;
+    }
+    if (callback_) {
+        callback_(data, reply);
+    }
+    done_ = true;
+    cv_.notify_one();
+    return TELEPHONY_ERR_SUCCESS;
+}
+
+bool RawParcelCallbackStub::WaitForResult(int64_t timeoutMs)
+{
+    std::unique_lock<std::mutex> lock(mtx_);
+    return cv_.wait_for(lock, std::chrono::microseconds(timeoutMs), [this] { return done_; });
+}
+
+} // namespace Telephony
+} // namespace OHOS
