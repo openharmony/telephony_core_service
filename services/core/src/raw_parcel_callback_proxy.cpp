22e30fd084926b6605b959ae7e3e8033a60d9922
xhttt123 <2317512661@qq.com>
2025-04-18 05:12:16 +0000

    add services/core/src/raw_parcel_callback_proxy.cpp.
    Signed-off-by: xhttt123 <2317512661@qq.com>

---
+++ services/core/src/raw_parcel_callback_proxy.cpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "raw_parcel_callback_proxy.h"
+#include "telephony_log_wrapper.h"
+
+namespace OHOS {
+namespace Telephony {
+void RawParcelCallbackProxy::Transfer(std::function<void(MessageParcel&)> func)
+{
+    MessageOption option{MessageOption::TF_ASYNC};
+    MessageParcel data;
+    MessageParcel reply;
+    sptr<IRemoteObject> remote = Remote();
+    if (remote == nullptr) {
+        TELEPHONY_LOGE("remote is nullptr!");
+        return;
+    }
+    if (!data.WriteInterfaceToken(GetDescriptor())) {
+        TELEPHONY_LOGE("write interface token failed");
+        return;
+    }
+    if (func != nullptr) {
+        func(data);
+    }
+    remote->SendRequest(0, data, reply, option);
+}
+} // namespace Telephony
+} // namespace OHOS
