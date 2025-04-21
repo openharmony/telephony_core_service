bc8f0ed45bacc2e6ae84b4e08c91a220e7ad1fba
xhttt123 <2317512661@qq.com>
2025-04-21 16:02:45 +0000

    update services/core/include/raw_parcel_callback_proxy.h.
    Signed-off-by: xhttt123 <2317512661@qq.com>

--- services/core/include/raw_parcel_callback_proxy.h
+++ services/core/include/raw_parcel_callback_proxy.h
@@ -23,8 +23,6 @@ public:
     explicit RawParcelCallbackProxy(const sptr<IRemoteObject> &impl)
         : IRemoteProxy<IRawParcelCallback>(impl) {}
     void Transfer(std::function<void(MessageParcel &)> func, MessageParcel &data) override;
-private:
-    static inline BrokerDelegator<RawParcelCallbackProxy> delegator_;
 };
 } // namespace Telephony
 } // namespace OHOS
