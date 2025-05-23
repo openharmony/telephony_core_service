517e831aca2b614345a7e9a1232f83c8ca824e64
刘峻朋 <liujunpeng20@huawei.com>
2025-05-23 09:08:36 +0000

    Vsim自愈
    Signed-off-by: 刘峻朋 <liujunpeng20@huawei.com>

--- services/network_search/src/network_search_manager.cpp
+++ services/network_search/src/network_search_manager.cpp
@@ -889,20 +889,19 @@ bool NetworkSearchManager::SetPreferredNetwork(int32_t slotId, int32_t networkMo
     return eventSender_->SendBase(slotId, RadioEvent::RADIO_SET_PREFERRED_NETWORK_MODE, filterMode);
 }
 
-bool NetworkSearchManager::SetForcePreferredNetwork(int32_t slotId, int32_t networkMode)
+bool NetworkSearchManager::SetForcePreferredNetwork(int32_t slotId, int32_t networkMode) 
 {
+    if (slotId < 0 || slotId > SIM_SLOT_COUNT) {
+        return false; 
+    } 
     if (simManager_ == nullptr || eventSender_ == nullptr) {
-        TELEPHONY_LOGE("simManager_ or eventSender_ is nullptr");
-        return false;
-    }
- 
-    int32_t raf = NetworkUtils::GetRafFromNetworkMode(static_cast<PreferredNetworkMode>(networkMode));
+        return false; 
+    } 
+    int32_t raf = NetworkUtils::GetRafFromNetworkMode(static_cast<PreferredNetworkMode>(networkMode)); 
     if (raf == static_cast<int32_t>(RadioProtocolTech::RADIO_PROTOCOL_TECH_UNKNOWN)) {
-        TELEPHONY_LOGE("SetPreferredNetwork failed raf: %{public}d slotId: %{public}d", raf, slotId);
-        return false;
-    }
-    TELEPHONY_LOGI("raf: %{public}d, networkMode: %{public}d slotId: %{public}d", raf, networkMode, slotId);
- 
+        return false; 
+    } 
+    TELEPHONY_LOGI("raf: %{public}d, networkMode: %{public}d slotId: %{public}d", raf, networkMode, slotId); 
     SetCachePreferredNetworkValue(slotId, networkMode);
     return eventSender_->SendBase(slotId, RadioEvent::RADIO_SET_PREFERRED_NETWORK_MODE, networkMode);
 }
