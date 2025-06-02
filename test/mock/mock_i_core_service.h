e5b05e2907c351ad45339d737de9f3f110c76e23
xhttt123 <2317512661@qq.com>
2025-06-02 20:35:10 +0800

    client
    Signed-off-by: xhttt123 <2317512661@qq.com>

--- test/mock/mock_i_core_service.h
+++ test/mock/mock_i_core_service.h
@@ -1,16 +1,18 @@
 /*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-Copyright (C) 2024 Huawei Device Co., Ltd.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 #ifndef MOCK_I_CORE_SERVICE_H
 #define MOCK_I_CORE_SERVICE_H
 
@@ -20,73 +22,74 @@ limitations under the License.
 
 namespace OHOS {
 namespace Telephony {
+
 class MockICoreService : public ICoreService {
 public:
-    MOCK_METHOD(sptr, AsObject, (), (override));
+    MOCK_METHOD(sptr<IRemoteObject>, AsObject, (), (override));
     MOCK_METHOD(int32_t, GetPsRadioTech, (int32_t slotId, int32_t &psRadioTech), (override));
     MOCK_METHOD(int32_t, GetCsRadioTech, (int32_t slotId, int32_t &csRadioTech), (override));
     MOCK_METHOD(std::u16string, GetOperatorNumeric, (int32_t slotId), (override));
     MOCK_METHOD(std::string, GetResidentNetworkNumeric, (int32_t slotId), (override));
     MOCK_METHOD(int32_t, GetOperatorName, (int32_t slotId, std::u16string &operatorName), (override));
-    MOCK_METHOD(int32_t, GetSignalInfoList, (int32_t slotId, std::vector<sptr> &signals), (override));
-    MOCK_METHOD(int32_t, GetNetworkState, (int32_t slotId, sptr &networkState), (override));
-    MOCK_METHOD(int32_t, SetRadioState, (int32_t slotId, bool isOn, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetRadioState, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetImei, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetImeiSv, (int32_t slotId, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, GetSignalInfoList, (int32_t slotId, std::vector<sptr<SignalInformation>> &signals), (override));
+    MOCK_METHOD(int32_t, GetNetworkState, (int32_t slotId, sptr<NetworkState> &networkState), (override));
+    MOCK_METHOD(int32_t, SetRadioState, (int32_t slotId, bool isOn, const sptr<INetworkSearchCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetRadioState, (int32_t slotId, const sptr<INetworkSearchCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetImei, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetImeiSv, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
     MOCK_METHOD(int32_t, GetMeid, (int32_t slotId, std::u16string &meid), (override));
     MOCK_METHOD(int32_t, GetUniqueDeviceId, (int32_t slotId, std::u16string &deviceId), (override));
     MOCK_METHOD(bool, IsNrSupported, (int32_t slotId), (override));
-    MOCK_METHOD(int32_t, SetNrOptionMode, (int32_t slotId, int32_t mode, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetNrOptionMode, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, HasSimCard, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetSimState, (int32_t slotId, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, SetNrOptionMode, (int32_t slotId, int32_t mode, const sptr<INetworkSearchCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetNrOptionMode, (int32_t slotId, const sptr<INetworkSearchCallback> &callback), (override));
+    MOCK_METHOD(int32_t, HasSimCard, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetSimState, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
     MOCK_METHOD(int32_t, GetDsdsMode, (int32_t &dsdsMode), (override));
     MOCK_METHOD(int32_t, GetCardType, (int32_t slotId, CardType &cardType), (override));
-    MOCK_METHOD(int32_t, UnlockPin, (int32_t slotId, const std::u16string &pin, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, UnlockPuk, (int32_t slotId, const std::u16string &newPin, const std::u16string &puk, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, AlterPin, (int32_t slotId, const std::u16string &newPin, const std::u16string &oldPin, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, UnlockPin2, (int32_t slotId, const std::u16string &pin2, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, UnlockPuk2, (int32_t slotId, const std::u16string &newPin2, const std::u16string &puk2, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, AlterPin2, (int32_t slotId, const std::u16string &newPin2, const std::u16string &oldPin2, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, SetLockState, (int32_t slotId, const LockInfo &options, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetLockState, (int32_t slotId, LockType lockType, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, UnlockPin, (int32_t slotId, const std::u16string &pin, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, UnlockPuk, (int32_t slotId, const std::u16string &newPin, const std::u16string &puk, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, AlterPin, (int32_t slotId, const std::u16string &newPin, const std::u16string &oldPin, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, UnlockPin2, (int32_t slotId, const std::u16string &pin2, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, UnlockPuk2, (int32_t slotId, const std::u16string &newPin2, const std::u16string &puk2, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, AlterPin2, (int32_t slotId, const std::u16string &newPin2, const std::u16string &oldPin2, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, SetLockState, (int32_t slotId, const LockInfo &options, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetLockState, (int32_t slotId, LockType lockType, const sptr<IRawParcelCallback> &callback), (override));
     MOCK_METHOD(int32_t, GetSimOperatorNumeric, (int32_t slotId, std::u16string &operatorNumeric), (override));
     MOCK_METHOD(int32_t, GetISOCountryCodeForSim, (int32_t slotId, std::u16string &countryCode), (override));
     MOCK_METHOD(int32_t, GetSimSpn, (int32_t slotId, std::u16string &spn), (override));
     MOCK_METHOD(int32_t, GetSimIccId, (int32_t slotId, std::u16string &iccId), (override));
     MOCK_METHOD(int32_t, GetIMSI, (int32_t slotId, std::u16string &imsi), (override));
-    MOCK_METHOD(int32_t, IsCTSimCard, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(bool, IsSimActive, (int32_t slotId, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, IsCTSimCard, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(bool, IsSimActive, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
     MOCK_METHOD(int32_t, GetSlotId, (int32_t simId), (override));
     MOCK_METHOD(int32_t, GetSimId, (int32_t slotId), (override));
-    MOCK_METHOD(int32_t, GetNetworkSearchInformation, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetNetworkSelectionMode, (int32_t slotId, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, GetNetworkSearchInformation, (int32_t slotId, const sptr<INetworkSearchCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetNetworkSelectionMode, (int32_t slotId, const sptr<INetworkSearchCallback> &callback), (override));
     MOCK_METHOD(std::u16string, GetLocaleFromDefaultSim, (), (override));
     MOCK_METHOD(int32_t, GetSimGid1, (int32_t slotId, std::u16string &gid1), (override));
     MOCK_METHOD(std::u16string, GetSimGid2, (int32_t slotId), (override));
     MOCK_METHOD(std::u16string, GetSimEons, (int32_t slotId, const std::string &plmn, int32_t lac, bool longNameRequired), (override));
     MOCK_METHOD(int32_t, SetNetworkSelectionMode, (int32_t slotId, int32_t selectMode,
-        const sptr &networkInformation, bool resumeSelection,
-        const sptr &callback), (override));
+        const sptr<NetworkInformation> &networkInformation, bool resumeSelection,
+        const sptr<INetworkSearchCallback> &callback), (override));
     MOCK_METHOD(int32_t, GetIsoCountryCodeForNetwork, (int32_t slotId, std::u16string &countryCode), (override));
     MOCK_METHOD(int32_t, GetSimAccountInfo, (int32_t slotId, IccAccountInfo &info), (override));
     MOCK_METHOD(int32_t, SetDefaultVoiceSlotId, (int32_t slotId), (override));
     MOCK_METHOD(int32_t, GetDefaultVoiceSlotId, (), (override));
-    MOCK_METHOD(int32_t, GetDefaultVoiceSimId, (const sptr &callback), (override));
+    MOCK_METHOD(int32_t, GetDefaultVoiceSimId, (const sptr<IRawParcelCallback> &callback), (override));
     MOCK_METHOD(int32_t, SetPrimarySlotId, (int32_t slotId), (override));
     MOCK_METHOD(int32_t, GetPrimarySlotId, (int32_t &slotId), (override));
-    MOCK_METHOD(int32_t, SetShowNumber, (int32_t slotId, const std::u16string &number, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetShowNumber, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, SetShowName, (int32_t slotId, const std::u16string &name, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetShowName, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, GetActiveSimAccountInfoList, (std::vector &iccAccountInfoList), (override));
+    MOCK_METHOD(int32_t, SetShowNumber, (int32_t slotId, const std::u16string &number, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetShowNumber, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, SetShowName, (int32_t slotId, const std::u16string &name, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetShowName, (int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
+    MOCK_METHOD(int32_t, GetActiveSimAccountInfoList, (std::vector<IccAccountInfo> &iccAccountInfoList), (override));
     MOCK_METHOD(int32_t, GetOperatorConfigs, (int32_t slotId, OperatorConfig &poc), (override));
     MOCK_METHOD(int32_t, RefreshSimState, (int32_t slotId), (override));
     MOCK_METHOD(int32_t, SetActiveSim, (int32_t slotId, int32_t enable), (override));
     MOCK_METHOD(int32_t, SetActiveSimSatellite, (int32_t slotId, int32_t enable), (override));
-    MOCK_METHOD(int32_t, GetPreferredNetwork, (int32_t slotId, const sptr &callback), (override));
-    MOCK_METHOD(int32_t, SetPreferredNetwork, (int32_t slotId, int32_t networkMode, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, GetPreferredNetwork, (int32_t slotId, const sptr<INetworkSearchCallback> &callback), (override));
+    MOCK_METHOD(int32_t, SetPreferredNetwork, (int32_t slotId, int32_t networkMode, const sptr<INetworkSearchCallback> &callback), (override));
     MOCK_METHOD(int32_t, GetNetworkCapability, (int32_t slotId, int32_t networkCapabilityType, int32_t &networkCapabilityState), (override));
     MOCK_METHOD(int32_t, SetNetworkCapability, (int32_t slotId, int32_t networkCapabilityType, int32_t networkCapabilityState), (override));
     MOCK_METHOD(int32_t, GetSimTelephoneNumber, (int32_t slotId, std::u16string &telephoneNumber), (override));
@@ -113,9 +116,9 @@ public:
     MOCK_METHOD(int32_t, GetCellInfoList, (int32_t slotId, std::vector<sptr> &cellInfo), (override));
     MOCK_METHOD(int32_t, GetNeighboringCellInfoList, (int32_t slotId, std::vector<sptr> &cellInfo), (override));
     MOCK_METHOD(int32_t, SendUpdateCellLocationRequest, (int32_t slotId), (override));
-    MOCK_METHOD(int32_t, HasOperatorPrivileges, (const int32_t slotId, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, HasOperatorPrivileges, (const int32_t slotId, const sptr<IRawParcelCallback> &callback), (override));
     MOCK_METHOD(int32_t, SimAuthentication, (int32_t slotId, AuthType authType, const std::string &authData, SimAuthenticationResponse &response), (override));
-    MOCK_METHOD(int32_t, RegisterImsRegInfoCallback, (int32_t slotId, ImsServiceType imsSrvType, const sptr &callback), (override));
+    MOCK_METHOD(int32_t, RegisterImsRegInfoCallback, (int32_t slotId, ImsServiceType imsSrvType, const sptr<INetworkSearchCallback> &callback), (override));
     MOCK_METHOD(int32_t, UnregisterImsRegInfoCallback, (int32_t slotId, ImsServiceType imsSrvType), (override));
     MOCK_METHOD(int32_t, GetBasebandVersion, (int32_t slotId, std::string &version), (override));
     MOCK_METHOD(int32_t, FactoryReset, (int32_t slotId), (override));
