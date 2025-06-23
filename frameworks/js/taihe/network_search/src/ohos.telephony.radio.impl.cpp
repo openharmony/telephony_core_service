4215bb6e3b31a667198591f440468fc3c721338f
gaozhichao <gaozhichao5@huawei.com>
2025-06-23 21:34:25 +0800

    [fix] 修改检视意见
    Signed-off-by: gaozhichao <gaozhichao5@huawei.com>

--- frameworks/js/taihe/network_search/src/ohos.telephony.radio.impl.cpp
+++ frameworks/js/taihe/network_search/src/ohos.telephony.radio.impl.cpp
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "ohos.telephony.radio.proj.hpp"
 #include "ohos.telephony.radio.impl.hpp"
+#include "ohos.telephony.radio.proj.hpp"
 #include "taihe/runtime.hpp"
 #include "stdexcept"
 #include "telephony_config.h"
