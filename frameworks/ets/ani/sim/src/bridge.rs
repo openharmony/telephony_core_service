70cefcdff97a77e15e37f723626e78b092f76938
wulei0903 <360633228@qq.com>
2025-09-03 19:27:04 +0800

    电话服务ArkTS接口整改
    Signed-off-by: wulei0903 <360633228@qq.com>

--- frameworks/ets/ani/sim/src/bridge.rs
+++ frameworks/ets/ani/sim/src/bridge.rs
@@ -28,6 +28,16 @@ impl From<i32> for LockState {
     }
 }
 
+impl From<LockState> for i32 {
+    fn from(value: LockState) -> Self {
+        match value {
+            LockState::LockOff => 0,
+            LockState::LockOn => 1,
+            _ => 0,
+        }
+    }
+}
+
 #[ani_rs::ani(path = "L@ohos/telephony/sim/sim/LockType")]
 #[repr(i32)]
 pub enum LockType {
@@ -79,6 +89,7 @@ pub struct AniOperatorConfig {
     field: String,
     value: String,
 }
+
 impl AniOperatorConfig {
     pub fn new(field: String, value: String) -> Self {
         Self { field, value }
@@ -183,4 +194,213 @@ impl From<i32> for SimState {
             _ => SimState::SimStateUnknown,
         }
     }
+}
+
+#[ani_rs::ani(path = "L@ohos/telephony/sim/sim/PersoLockType")]
+#[repr(i32)]
+pub enum PersoLockType {
+    PnPinLock = 0,
+    PnPukLock = 1,
+    PuPinLock = 2,
+    PuPukLock = 3,
+    PpPinLock = 4,
+    PpPukLock = 5,
+    PcPinLock = 6,
+    PcPukLock = 7,
+    SimPinLock = 8,
+    SimPukLock = 9,
+}
+
+impl From<PersoLockType> for i32 {
+    fn from(value: PersoLockType) -> Self {
+        match value {
+            PersoLockType::PnPinLock => 0,
+            PersoLockType::PnPukLock => 1,
+            PersoLockType::PuPinLock => 2,
+            PersoLockType::PuPukLock => 3,
+            PersoLockType::PpPinLock => 4,
+            PersoLockType::PpPukLock => 5,
+            PersoLockType::PcPinLock => 6,
+            PersoLockType::PcPukLock => 7,
+            PersoLockType::SimPinLock => 8,
+            PersoLockType::SimPukLock => 9,
+            _ => 0,
+        }
+    }
+}
+
+#[ani_rs::ani(path = "L@ohos/telephony/sim/sim/PersoLockInfoInner")]
+pub struct AniPersoLockInfo {
+    pub lock_type: PersoLockType,
+    pub password: String,
+}
+
+impl AniPersoLockInfo {
+    pub fn new() -> Self {
+        Self {
+            lock_type: PersoLockType::PnPinLock,
+            password: String::new(),
+        }
+    }
+}
+
+#[ani_rs::ani(path = "L@ohos/telephony/sim/sim/ContactType")]
+#[repr(i32)]
+pub enum ContactType {
+    GeneralContact = 1,
+    FixedDialing = 2,
+}
+
+impl From<ContactType> for i32 {
+    fn from(value: ContactType) -> Self {
+        match value {
+            ContactType::GeneralContact => 1,
+            ContactType::FixedDialing => 2,
+            _ => 1,
+        }
+    }
+}
+
+#[ani_rs::ani(path="L@ohos/telephony/sim/sim/LockInfoInner")]
+pub struct AniLockInfo {
+    pub lock_type: LockType,
+    pub password: String,
+    pub state: LockState,
+}
+
+impl AniLockInfo {
+    pub fn new() -> Self {
+        Self {
+            lock_type: LockType::PinLock,
+            password: String::new(),
+            state: LockState::LockOff,
+        }
+    }
+}
+
+#[ani_rs::ani(path="L@ohos/telephony/sim/sim/CardType")]
+#[repr(i32)]
+pub enum CardType {
+    UnknownCard = -1,
+
+    SingleModeSimCard = 10,
+
+    SingleModeUsimCard = 20,
+
+    SingleModeRuimCard = 30,
+    
+    DualModeCgCard = 40,
+
+    CtNationalRoamingCard = 41,
+
+    CuDualModeCard = 42,
+
+    DualModeTelecomLteCard = 43,
+
+    DualModeUgCard = 50,
+
+    SingleModeIsimCard = 60,
+}
+
+impl From<i32> for CardType {
+    fn from(value: i32) -> Self {
+        match value {
+            -1 => CardType::UnknownCard,
+            10 => CardType::SingleModeSimCard,
+            20 => CardType::SingleModeUsimCard,
+            30 => CardType::SingleModeRuimCard,
+            40 => CardType::DualModeCgCard,
+            41 => CardType::CtNationalRoamingCard,
+            42 => CardType::CuDualModeCard,
+            43 => CardType::DualModeTelecomLteCard,
+            50 => CardType::DualModeUgCard,
+            60 => CardType::SingleModeIsimCard,
+            _ => CardType::UnknownCard,
+        }
+    }
+}
+
+#[ani_rs::ani(path="L@ohos/telephony/sim/sim/OperatorSimCard")]
+#[repr(i32)]
+pub enum OperatorSimCard {
+    ChinaTelecomCard = 0,
+}
+
+impl OperatorSimCard {
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            OperatorSimCard::ChinaTelecomCard => "china_telecom_card",
+        }
+    }
+}
+
+#[ani_rs::ani(path="L@ohos/telephony/sim/sim/DsdsMode")]
+#[repr(i32)]
+pub enum DsdsMode {
+    DsdsModeV1 = 0,
+
+    DsdsModeV3 = 1,
+
+    DsdsModeV5Tdm = 2,
+
+    DsdsModeV5Dsda = 3,
+}
+
+impl From<i32> for DsdsMode {
+    fn from(value: i32) -> Self {
+        match value {
+            0 => DsdsMode::DsdsModeV1,
+            1 => DsdsMode::DsdsModeV3,
+            2 => DsdsMode::DsdsModeV5Tdm,
+            3 => DsdsMode::DsdsModeV5Dsda,
+            _ => DsdsMode::DsdsModeV1,
+        }
+    }
+}
+
+#[ani_rs::ani(path="L@ohos/telephony/sim/sim/AuthType")]
+#[repr(i32)]
+pub enum AuthType {
+    SimAuthEapSimtype = 128,
+
+    SimAuthEapAkaType = 129,
+}
+
+impl From<AuthType> for i32 {
+    fn from(value: AuthType) -> Self {
+        match value {
+            AuthType::SimAuthEapSimtype => 128,
+            AuthType::SimAuthEapAkaType => 129,
+            _ => 128,
+        }
+    }
+}
+
+#[ani_rs::ani(path="L@ohos/telephony/sim/sim/SimAuthenticationResponseInner")]
+pub struct AniSimAuthenticationResponse {
+    pub sim_status_word1: i32,
+
+    pub sim_status_word2: i32,
+  
+    pub response: String,
+}
+
+impl AniSimAuthenticationResponse {
+    pub fn new() -> Self {
+        Self {
+            sim_status_word1: 0,
+            sim_status_word2: 0,
+            response: String::new(),
+        }
+    }
+}
+
+pub fn sim_authentication_response_conversion(
+    sim_authentication_response: &mut AniSimAuthenticationResponse,
+    status_word1: i32,
+    status_word2: i32,
+    response: String) {
+    sim_authentication_response.sim_status_word1 = status_word1;
+    sim_authentication_response.sim_status_word2 = status_word2;
+    sim_authentication_response.response = response;
 }
