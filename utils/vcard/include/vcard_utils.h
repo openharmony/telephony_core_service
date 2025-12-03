bfa94ed13efdc7d9a16da01350c4f00bb399c87b
huzike9614 <huzijian@huawei.com>
2025-12-03 18:08:35 +0800

    update: 更新文件 vcard_utils.h 修改建议
    Signed-off-by: huzike9614 <huzijian@huawei.com>

--- utils/vcard/include/vcard_utils.h
+++ utils/vcard/include/vcard_utils.h
@@ -62,8 +62,8 @@ public:
     static bool IsContainsInvisibleChar(const std::string& value);
     static bool IsChineseString(const std::string& value);
     static size_t GetOStreamSize(const std::ostringstream &oss);
-    static bool isEnAscii(char c);
-    static bool isEnPrintableAscii(const std::string &str);
+    static bool IsPrintableChar(char c);
+    static bool IsPrintableString(const std::string &str);
 };
 } // namespace Telephony
 } // namespace OHOS
