b1c2fbecf548d8d0d0695594246d515694a31d89
dzw_1 <dujiawang@h-partners.com>
2025-12-17 09:08:55 +0800

    fuzz fail modify
    Signed-off-by: dzw_1 <dujiawang@h-partners.com>

--- test/fuzztest/simauthentication_fuzzer/simauthentication_fuzzer.cpp
+++ test/fuzztest/simauthentication_fuzzer/simauthentication_fuzzer.cpp
@@ -182,6 +182,5 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     /* Run your code on data */
     OHOS::DoSomethingInterestingWithMyAPI(data, size);
-    OHOS::DelayedSingleton<CoreService>::DestroyInstance();
     return 0;
 }
