c317e9532ff7b3a02f85711845ead5aa2ced1af9
haige1234 <lijianghai3@h-partners.com>
2025-12-31 16:13:46 +0800

    fix fuzz fail
    Signed-off-by: haige1234 <lijianghai3@h-partners.com>

--- test/fuzztest/networksearch_fuzzer/networksearch_fuzzer.cpp
+++ test/fuzztest/networksearch_fuzzer/networksearch_fuzzer.cpp
@@ -222,19 +222,6 @@ void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
     NetworkSearchHandlerEvents(data, size);
     NetworkSearchHandlerProcesses(data, size);
     NetworkSearchHandlerGetRegistration(data, size);
-
-    auto telRilManager = std::static_pointer_cast<TelRilManager>(
-         DelayedSingleton<CoreService>::GetInstance()->telRilManager_);
-    if (telRilManager == nullptr || telRilManager->handler_ == nullptr) {
-        return;
-    }
-    auto handler = telRilManager->handler_;
-    if (handler != nullptr) {
-        handler->RemoveAllEvents();
-        usleep(SLEEP_TIME_SECONDS);
-    }
-    telRilManager->handler_->ClearFfrt(false);
-    telRilManager->handler_->queue_ = nullptr;
     return;
 }
 } // namespace OHOS
--- test/fuzztest/voicemailconstants_fuzzer/voicemailconstants_fuzzer.cpp
+++ test/fuzztest/voicemailconstants_fuzzer/voicemailconstants_fuzzer.cpp
@@ -58,18 +58,6 @@ void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
     }
  
     VoiceMailConstantseFunc(data, size);
-    auto telRilManager = std::static_pointer_cast<TelRilManager>(
-         DelayedSingleton<CoreService>::GetInstance()->telRilManager_);
-    if (telRilManager == nullptr || telRilManager->handler_ == nullptr) {
-        return;
-    }
-    auto handler = telRilManager->handler_;
-    if (handler != nullptr) {
-        handler->RemoveAllEvents();
-        usleep(SLEEP_TIME_SECONDS);
-    }
-    telRilManager->handler_->ClearFfrt(false);
-    telRilManager->handler_->queue_ = nullptr;
     return;
 }
 } // namespace OHOS
