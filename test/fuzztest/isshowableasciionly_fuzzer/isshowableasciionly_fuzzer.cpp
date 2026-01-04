562acde79a3a1a1c1aa2f34cdadf7834919f98fc
JW_NNNNN <niujiawei2@huawei.com>
2026-01-04 11:43:06 +0800

    update: 更新文件 isshowableasciionly_fuzzer.cpp
    Signed-off-by: JW_NNNNN <niujiawei2@huawei.com>

--- test/fuzztest/isshowableasciionly_fuzzer/isshowableasciionly_fuzzer.cpp
+++ test/fuzztest/isshowableasciionly_fuzzer/isshowableasciionly_fuzzer.cpp
@@ -38,7 +38,7 @@ void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
     int32_t offset = 0;
     std::string str(reinterpret_cast<const char *>(data), size);
     offset += sizeof(int32_t);
-    offset = (offset > (int32_t)size) ? (int32_t)size : offset;
+    offset = (offset > static_cast<int32_t>size) ? static_cast<int32_t>size : offset;
     std::string parameter(reinterpret_cast<const char *>(data + offset), size - offset);
     simUtils->HexStringConvertToBytes(str, byteslen);
     simUtils->IsShowableAsciiOnly(str);
