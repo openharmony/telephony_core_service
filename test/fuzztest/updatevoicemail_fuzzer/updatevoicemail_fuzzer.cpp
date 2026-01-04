c1f95285b2821bb8b6a43dc33633678e84d62eab
JW_NNNNN <niujiawei2@huawei.com>
2026-01-04 10:10:21 +0800

    update: 更新文件 updatevoicemail_fuzzer.cpp
    Signed-off-by: JW_NNNNN <niujiawei2@huawei.com>

--- test/fuzztest/updatevoicemail_fuzzer/updatevoicemail_fuzzer.cpp
+++ test/fuzztest/updatevoicemail_fuzzer/updatevoicemail_fuzzer.cpp
@@ -49,9 +49,12 @@ void DoSomethingInterestingWithMyAPI(const uint8_t *data, size_t size)
     std::int32_t eventId = static_cast<int32_t>(*data + offset);
     offset += sizeof(int32_t);
     std::int64_t refId = static_cast<int64_t>(*data + offset);
-    std::string operatorNum(reinterpret_cast<const char *>(data), size);
-    std::string mailName(reinterpret_cast<const char *>(data), size);
-    std::string mailNumber(reinterpret_cast<const char *>(data), size);
+    offset += sizeof(int32_t);
+    std::string operatorNum(reinterpret_cast<const char *>(data + offset), size);
+    offset += sizeof(int32_t);
+    std::string mailName(reinterpret_cast<const char *>(data + offset), size);
+    offset += sizeof(int32_t);
+    std::string mailNumber(reinterpret_cast<const char *>(data + offset), size);
     AppExecFwk::InnerEvent::Pointer event = AppExecFwk::InnerEvent::Get(eventId, refId);
     auto telRilManager_ = std::make_shared<TelRilManager>();
     auto stateManager_ = std::make_shared<SimStateManager>(telRilManager_);
