187e706f6262c2a1b9c568f9fec1899687a2aa4a
JW_NNNNN <niujiawei2@huawei.com>
2026-01-04 11:49:41 +0800

    update: 更新文件 setvoicemailinfo_fuzzer.cpp
    Signed-off-by: JW_NNNNN <niujiawei2@huawei.com>

--- test/fuzztest/setvoicemailinfo_fuzzer/setvoicemailinfo_fuzzer.cpp
+++ test/fuzztest/setvoicemailinfo_fuzzer/setvoicemailinfo_fuzzer.cpp
@@ -113,8 +113,8 @@ void SetVoiceMailInfo(const uint8_t *data, size_t size)
     int32_t slotId = static_cast<int32_t>(*data % SLOT_NUM);
     int32_t offset = 0;
     std::string mailNumber(reinterpret_cast<const char *>(data), size);
-    offset += sizeof(int32_t)
-    offset = (offset > (int32_t)size) ? (int32_t)size : offset;
+    offset += sizeof(int32_t);
+    offset = (offset > static_cast<int32_t>size) ? static_cast<int32_t>size : offset;
     std::string mailName(reinterpret_cast<const char *>(data + offset), size - offset);
     auto mailNameU16 = Str8ToStr16(mailName);
     auto mailNumberU16 = Str8ToStr16(mailNumber);
