faaa91bcb1bc8449a57c2b168dc9f48c4f004f94
hhd227 <3216023252@qq.com>
2026-01-19 15:57:46 +0800

    update: 更新文件 multi_sim_monitor.h
    Signed-off-by: hhd227 <3216023252@qq.com>

--- services/sim/include/multi_sim_monitor.h
+++ services/sim/include/multi_sim_monitor.h
@@ -57,7 +57,7 @@ public:
     void SetLastUserId(int32_t userId);
     void UpdateAllSimData(int32_t userId);
     void OnUserSwitched(int32_t userId);
-    bool IsUserIdValid();
+    void OnDataShareReady(int32_t userId);
     void CheckSimPresentWhenReboot();
     inline void SetOperatorConfigHisysevent(std::weak_ptr<IOperatorConfigHisysevent> operatorConfigHisysevent)
     {
