b71ebe2f3aeda1ed7af6a492b65fd40950bb9232
hu-peiqi <hupeiqi2@huawei.com>
2026-02-06 09:59:40 +0800

    update: 更新文件 network_search_state.cpp
    Signed-off-by: hu-peiqi <hupeiqi2@huawei.com>

--- services/network_search/src/network_search_state.cpp
+++ services/network_search/src/network_search_state.cpp
@@ -479,7 +479,7 @@ void NetworkSearchState::NotifyStateChange()
                 networkStateOld_->Marshalling(data);
                 networkState_->ReadFromParcel(data);
                 processNetworkState_ = true;
-                TELEPHONY_LOGI("NotifyStateChange slotId:%{public}d, regStatus:%{public}d, isSame:%{public}d",
+                TELEPHONY_LOGI("NotifyStateChange delay slotId:%{public}d, regStatus:%{public}d, isSame:%{public}d",
                     slotId_, networkState_->GetRegStatus(), *networkState_ == *networkStateOld_);
                 return;
             }
@@ -503,7 +503,7 @@ void NetworkSearchState::NotifyStateChange()
         networkState_->Marshalling(data);
         networkStateOld_->ReadFromParcel(data);
     }
-    TELEPHONY_LOGI("NotifyStateChange slotId:%{public}d, regStatus:%{public}d, isSame:%{public}d",
+    TELEPHONY_LOGI("NotifyStateChange finish slotId:%{public}d, regStatus:%{public}d, isSame:%{public}d",
         slotId_, networkState_->GetRegStatus(), *networkState_ == *networkStateOld_);
     processNetworkState_ = false;
 }
