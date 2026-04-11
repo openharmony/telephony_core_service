2a80c980ed3c2c418eada7055a18e6f46b91b170
hhd227 <huanghaodong9@h-partners.com>
2026-04-11 09:26:03 +0800

    update: 更新文件 multi_sim_monitor.cpp
    Signed-off-by: hhd227 <huanghaodong9@h-partners.com>

--- services/sim/src/multi_sim_monitor.cpp
+++ services/sim/src/multi_sim_monitor.cpp
@@ -549,8 +549,9 @@ bool MultiSimMonitor::IsUserIdRecord(int32_t userId)
 
 void MultiSimMonitor::ClearUserId()
 {
-    std::lock_guard<std::mutex> lock(mutexForUserId_);
-    userIdRecord_.fill(0);
+    for (int i = 0; i < MAX_USERID_NUM; i++) {
+        userIdRecord_[i] = 0;
+    }
 }
 
 void MultiSimMonitor::SetLastUserId(int32_t userId)
@@ -560,7 +561,6 @@ void MultiSimMonitor::SetLastUserId(int32_t userId)
     if (userId == 0) {
         return;
     }
-    std::lock_guard<std::mutex> lock(mutexForUserId_);
     isRecord = IsUserIdRecord(userId);
     if (!isRecord) {
         if (userId == NORMAL_USERID) {
@@ -569,13 +569,12 @@ void MultiSimMonitor::SetLastUserId(int32_t userId)
         }
         userIdRecord_[userIdRecordIndex_++] = userId;
         userIdRecordIndex_ = userIdRecordIndex_ % MAX_USERID_NUM;
-        userIdRecordIndex_ = userIdRecordIndex_ == 0 ? userIdRecordIndex_ + 1 : userIdRecordIndex_;
+        userIdRecordIndex_ = userIdRecordIndex_ == 0 ? userIdRecordIndex_ + 1 : userIdRecordIndex_.load();
     }
 }
 
 void MultiSimMonitor::UpdateAllSimData(int32_t userId)
 {
-    std::lock_guard<std::mutex> lock(mutexForUserId_);
     bool isRecord = IsUserIdRecord(userId);
     if (!isRecord) {
         SendEvent(MultiSimMonitor::RESET_OPKEY_CONFIG);
