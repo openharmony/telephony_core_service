470ffdbc14823279831059e0c3533246d5a0748b
Yiming_Lv <2909256199@qq.com>
2026-08-01 10:58:47 +0800

    update: 更新文件 sim_state_type.h
    Signed-off-by: Yiming_Lv <2909256199@qq.com>

--- interfaces/innerkits/include/sim_state_type.h
+++ interfaces/innerkits/include/sim_state_type.h
@@ -355,7 +355,7 @@ struct SimLabel {
     {
         return (this->simType == p.simType && (p.simType ==SimType::ESIM || this->index == p.index));
     }
-        bool operator>(const SimLabel &p) const
+    bool operator>(const SimLabel &p) const
     {
         if (this->simType == SimType::PSIM && p.simType == SimType::ESIM) {
             return true;
