697d3435cc994779ffd2f7787f219e94f2f4566b
lizhiqi_hw <lizhiqi14@huawei.com>
2026-09-05 11:38:18 +0800

    update: 更新文件 network_search_state.cpp
    Signed-off-by: lizhiqi_hw <lizhiqi14@huawei.com>

--- services/network_search/src/network_search_state.cpp
+++ services/network_search/src/network_search_state.cpp
@@ -474,7 +474,7 @@ void NetworkSearchState::NotifyImsStateChange(ImsServiceType imsSrvType, const I
 
 void NetworkSearchState::NotifyStateChange()
 {
-    std::unique_lock<std::mutex> lock(mutex_);
+    std::unique_lock<ffrt::mutex> lock(mutex_);
     HILOG_COMM_INFO("NetworkSearchState::NotifyStateChange slotId:%{public}d", slotId_);
     if (networkState_ == nullptr) {
         TELEPHONY_LOGE("NotifyStateChange networkState_ is null slotId:%{public}d", slotId_);
